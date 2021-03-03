45e0634a0607fedfa2111618306218101152d4ab
Sylvain Chauffaille <contact@sylchauf.net>
2021-03-03 11:22:28 +0100

    Initial commit : first test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+import SendBirdCall from "sendbird-calls";
+
+/**
+ * Variables Sendbird App
+ * @TODO replace by the good value
+ */
+const APP_ID = "SENDBIRD_APP_ID";
+const AGENT_ID = "AGENT_USER_ID";
+const USER_ID = 208;
+
+let call = null;
+
+SendBirdCall.init(APP_ID);
+
+const App = () => {
+  const [inCall, setInCall] = useState(false);
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  const init = async () => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const access = SendBirdCall.useMedia({ audio: true, video: true });
+
+    const authOption = { userId: USER_ID };
+
+    return SendBirdCall.authenticate(authOption, (result, error) => {
+      if (error) console.error(error);
+      else {
+        // The user has been successfully authenticated and is connected to Sendbird server.
+        console.log("Authenticated");
+
+        // Establishing websocket connection.
+        SendBirdCall.connectWebSocket().then(() => {
+          console.log("Websocket connected");
+        });
+      }
+    });
+  };
+
+  const makeCall = async () => {
+    if (call) return;
+
+    const dialParams = {
+      userId: AGENT_ID,
+      isVideoCall: true,
+      callOption: {
+        localMediaView: document.getElementById("localVideo"),
+        remoteMediaView: document.getElementById("remoteVideo"),
+        audioEnabled: true,
+        videoEnabled: true,
+      },
+    };
+
+    call = SendBirdCall.dial(dialParams, (call, error) => {
+      if (error) console.error(error);
+
+      // Dialing succeeded.
+      console.log("Ringing !");
+    });
+
+    call.onEstablished = () => setInCall(true);
+    call.onEnded = () => {
+      call = null;
+    };
+  };
+
+  const handleEndCall = async () => {
+    if (call) call.end();
+  };
+
+  const handleChangeCamera = async () => {
+    const result = SendBirdCall.getAvailableVideoInputDevices();
+
+    const otherCam = result.find((i) => i.label.includes("back"));
+
+    SendBirdCall.selectVideoInputDevice(otherCam);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        {inCall ? (
+          <button onClick={handleEndCall}>Stop call</button>
+        ) : (
+          <button onClick={() => makeCall()}>Start call</button>
+        )}
+        {inCall && (
+          <button onClick={() => handleChangeCamera()}>Change camera</button>
+        )}
+      </div>
+      <video id={"localVideo"} autoPlay muted></video>
+      <video id={"remoteVideo"} autoPlay></video>
     </div>
   );
-}
+};
 
 export default App;
